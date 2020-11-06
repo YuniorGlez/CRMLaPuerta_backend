5d027ae7b8e5229e8106b0e9ced4d29d336c85dd
Yunior Gonzalez <yunior@reboot.academy>
2020-11-06 15:52:07 +0000

    Añadido express y comprobado pimer endpoint

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('Hola mundo')
+const express = require('express');
+const app = express();
+
+
+
+
+app.get('/', (request, response) => {
+    
+    return response.send('Hola Mundo');
+})
+
+
+
+
+
+
+
+app.listen(5000, (err) => {
+    if (err) return console.log('Hay una error ! ', err);
+    console.log('Servidor corriendo en el 5000');
+})
