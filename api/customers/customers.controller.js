c4d4e0624cedb580856e0970c1a4b9e7c9bc2254
Yunior Gonzalez <yunior@reboot.academy>
2020-11-06 16:33:47 +0000

    Tenemos un controller con todos los métodos posibles enlazados. Sin implementar, solo mock

---
+++ api/customers/customers.controller.js
@@ -0,0 +1,35 @@
+/**
+ * GET     /customers              ->  getAll
+ * GET     /customers/:id          ->  getOneById
+ * POST    /customers              ->  createOne
+ * put     /customers/:id          ->  putOne
+ * patch     /customers/:id          ->  patchOne
+ * delete     /customers/:id          ->  removeOne
+ **/
+
+module.exports.getAll = getAll;
+module.exports.getOneById = getOneById;
+module.exports.createOne = createOne;
+module.exports.putOne = putOne;
+module.exports.patchOne = patchOne;
+module.exports.removeOne = removeOne;
+
+function getAll ( req, res ) {
+    return res.send('Todo salió bien, me hicise un getAll')
+}
+function getOneById ( req, res ) {
+    return res.send('Todo salió bien, me hicise un getOneById')
+}
+function createOne ( req, res ) {
+    return res.send('Todo salió bien, me hicise un createOne')
+}
+function putOne ( req, res ) {
+    return res.send('Todo salió bien, me hicise un putOne')
+}
+function patchOne ( req, res ) {
+    return res.send('Todo salió bien, me hicise un patchOne')
+}
+function removeOne ( req, res ) {
+    return res.send('Todo salió bien, me hicise un removeOne')
+}
+
--- api/customers/customers.router.js
+++ api/customers/customers.router.js
@@ -1,9 +1,11 @@
 const router = require('express').Router()
+const controller = require('./customers.controller');
 
-router.get('/', (req, res) => {
-    res.json([]);
-} ) 
-
-
+router.get('/', controller.getAll ) 
+router.get('/:id', controller.getOneById ) 
+router.post('/', controller.createOne ) 
+router.put('/:id', controller.putOne ) 
+router.patch('/:id', controller.patchOne ) 
+router.delete('/:id', controller.removeOne ) 
 
 module.exports = router;
