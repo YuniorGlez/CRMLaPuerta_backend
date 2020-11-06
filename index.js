8d2e2f3dd52c7af38548171fb8d1327c50ae68d3
Yunior Gonzalez <yunior@reboot.academy>
2020-11-06 15:58:31 +0000

    Permitimos bodies con JSON y permitimos peticiones de origenes externos con cors

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+ 
+app.use(cors({ origin : [  'osama.com' , 'localhost:4200'  ]}));
 
-
-
+app.use(  express.json()  );
 
 app.get('/', (request, response) => {
-    
     return response.send('Hola Mundo');
 })
-
-
-
-
-
 
 
 app.listen(5000, (err) => {
